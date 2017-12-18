937804f01bf44d99d35ea5c1b60e5086fb3b9802
Miguel Angel Quinones <miguel@miqu.me>
2017-12-18 13:12:06 +0000

    [ADD] Framework support for Carthage

---
+++ Frameworks/SnowplowTracker/SnowplowTracker.h
@@ -0,0 +1,27 @@
+//
+//  SnowplowTracker.h
+//  SnowplowTracker
+//
+//  Created by Miguel on 18/12/2017.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SnowplowTracker.
+FOUNDATION_EXPORT double SnowplowTrackerVersionNumber;
+
+//! Project version string for SnowplowTracker.
+FOUNDATION_EXPORT const unsigned char SnowplowTrackerVersionString[];
+
+
+#import <SnowplowTracker/Snowplow.h>
+#import <SnowplowTracker/SPTracker.h>
+#import <SnowplowTracker/SPEmitter.h>
+#import <SnowplowTracker/SPSubject.h>
+#import <SnowplowTracker/SPPayload.h>
+#import <SnowplowTracker/SPSelfDescribingJson.h>
+#import <SnowplowTracker/SPUtilities.h>
+#import <SnowplowTracker/SPRequestCallback.h>
+#import <SnowplowTracker/SPEvent.h>
+
+
